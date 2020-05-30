e62bd33a179500fac17befd51d41841f4aa27a6b
MGrasdal <martin_grasdal@hotmail.com>
2020-05-30 17:30:35 -0600

    Update Install-WebApp.ps1

--- Labs/AIS/Install-WebApp.ps1
+++ Labs/AIS/Install-WebApp.ps1
@@ -5,4 +5,4 @@ netsh advfirewall firewall add rule name="http" dir=in action=allow protocol=TCP
 Install-WindowsFeature Web-Server -IncludeManagementTools
 
 # Copy new index.html file
-Invoke-WebRequest 'https://raw.githubusercontent.com/grabinski/ais-006/master/index.html' -OutFile 'C:\inetpub\wwwroot\index.html'
+Invoke-WebRequest 'https://raw.githubusercontent.com/LODSContent/ChallengeLabs_ArmResources/master/Labs/AIS/index.html' -OutFile 'C:\inetpub\wwwroot\index.html'
