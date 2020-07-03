6e8f49084ec9c2281438ee595418c0569635b0c2
MGrasdal <martin_grasdal@hotmail.com>
2020-07-03 11:14:17 -0600

    Add files via upload

---
+++ Labs/lods-cs001/admin/CSST08Script.ps1
@@ -0,0 +1,22 @@
+﻿$rg = "CSSClod6823018"
+$LabID = "6823018"
+$cdbName = "cdb$($LabID)"
+$webAppName = "lods$($LabID)"
+$WebUri = "$($webAppName).azurewebsites.net/api/test/loadcosmosdb"
+
+$key = (Invoke-AzureRmResourceAction -Action listKeys `
+    -ResourceType "Microsoft.DocumentDb/databaseAccounts" `
+    -ApiVersion "2015-04-08" `
+    -ResourceGroupName $rg `
+    -Force `
+    -Name $cdbName).PrimaryMasterKey
+
+
+$appSettings = @{
+    "ListingsURI" = "https://$($cdbName).documents.azure.com:443";
+    "ListingsKey" = $key;
+    "TestType" = "32"
+}
+
+Set-AzureRmWebApp -ResourceGroupName $rg -Name $webAppName -AppSettings $appSettings
+Invoke-RestMethod -Method Get -Uri $WebUri 
